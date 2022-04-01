f3021547b1b3f82411f5875258365a7f57e2206d
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-01 16:56:03 -0300

    Esqueleto do PlaylistManager

---
+++ Salamandra.Engine/Services/PlaylistManager.cs
@@ -0,0 +1,31 @@
+﻿using Salamandra.Engine.Domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Services
+{
+    public class PlaylistManager
+    {
+        public ObservableCollection<SoundFileTrack> Tracks { get; set; }
+
+        public SoundFileTrack CurrentTrack { get; set; }
+        public SoundFileTrack NextTrack { get; set; }
+
+        public PlaylistManager()
+        {
+            this.Tracks = new ObservableCollection<SoundFileTrack>();
+
+            this.CurrentTrack = null;
+            this.NextTrack = null;
+        }
+
+        public void UpdateNextTrack()
+        {
+
+        }
+    }
+}

8340db1586e9ddd39de37d7c2e04bd9dec604333
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-01 16:46:56 -0300

    Classe SoundFileTrack

---
+++ Salamandra.Engine/Domain/SoundFileTrack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain
+{
+    public class SoundFileTrack
+    {
+        public string Filename { get; private set; }
+
+        public SoundFileTrack(string filename)
+        {
+            this.Filename = filename;
+        }
+    }
+}

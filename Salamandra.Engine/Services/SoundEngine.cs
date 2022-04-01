1b7af4e1f0ffe3f69636a440f0b08da621bcac7f
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-01 16:19:06 -0300

    Esqueleto do SoundEngine

---
+++ Salamandra.Engine/Services/SoundEngine.cs
@@ -0,0 +1,30 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Services
+{
+    public class SoundEngine
+    {
+        private WaveOutEvent waveOutEvent;
+        private AudioFileReader audioFileReader;
+
+        public SoundEngine()
+        {
+        }
+
+        public void PlayAudioFile(string filename)
+        {
+
+        }
+
+
+        public void Stop()
+        {
+
+        }
+    }
+}

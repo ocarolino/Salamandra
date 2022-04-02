121ef454628af35ffa865c1632679bfe4e80b7d0
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-02 12:20:41 -0300

    SameObjectToBoolConverter e estilo de próxima faixa

---
+++ Salamandra/Converters/SameObjectToBoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Salamandra.Converters
+{
+    public class SameObjectToBoolConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2)
+                throw new ArgumentException("Only two objects must be passed to this converter.");
+
+            if (values[0] == null || values[1] == null)
+                return false;
+
+            return values[0].Equals(values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

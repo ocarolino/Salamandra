9b8e45eabd1dcc8935c4a2b65f09f726d8a83a1b
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-02 09:29:47 -0300

    Implementação básica do PlaylistManager

---
+++ Salamandra.Engine/Domain/PlaylistMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain
+{
+    public enum PlaylistMode
+    {
+        Default = 0,
+        Repeat = 1,
+        Shuffle = 2,
+    }
+}
--- Salamandra.Engine/Services/PlaylistManager.cs
+++ Salamandra.Engine/Services/PlaylistManager.cs
@@ -10,6 +10,7 @@ namespace Salamandra.Engine.Services
 {
     public class PlaylistManager
     {
+        public PlaylistMode PlaylistMode { get; set; }
         public ObservableCollection<SoundFileTrack> Tracks { get; set; }
 
         public SoundFileTrack CurrentTrack { get; set; }
@@ -25,7 +26,36 @@ namespace Salamandra.Engine.Services
 
         public void UpdateNextTrack()
         {
+            if (this.Tracks.Count == 0)
+            {
+                this.NextTrack = null;
+                return;
+            }    
 
+            int nextTrackIndex = this.Tracks.IndexOf(this.CurrentTrack) + 1;
+
+            switch (this.PlaylistMode)
+            {
+                case PlaylistMode.Default:
+                case PlaylistMode.Repeat:
+                    if (nextTrackIndex >= this.Tracks.Count)
+                    {
+                        if (this.PlaylistMode == PlaylistMode.Default)
+                            this.NextTrack = null;
+                        else
+                            this.NextTrack = this.Tracks.First();
+                    }
+                    else
+                        this.NextTrack = this.Tracks[nextTrackIndex];
+                    break;
+                case PlaylistMode.Shuffle:
+                    Random random = new Random(); // ToDo: Random singleton.
+
+                    this.NextTrack = this.Tracks[random.Next(0, this.Tracks.Count)];
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }

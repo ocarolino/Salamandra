59e8f52953caab0715f13b08975bfb05e8063439
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-04 21:04:34 -0300

    AddTracks continuava setando a próxima faixa mesmo que estivesse já setada

--- Salamandra.Engine/Services/PlaylistManager.cs
+++ Salamandra.Engine/Services/PlaylistManager.cs
@@ -66,7 +66,7 @@ namespace Salamandra.Engine.Services
             foreach (var item in tracks)
                 this.Tracks.Add(item);
 
-            if (this.CurrentTrack == null)
+            if (this.NextTrack == null)
                 UpdateNextTrack();
         }
 

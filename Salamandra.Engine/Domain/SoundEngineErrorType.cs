e26507ad7750ecffcd1fe2283097e94e42c73a3a
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-06 19:24:48 -0300

    Enum de SoundEngineErrorType

---
+++ Salamandra.Engine/Domain/SoundEngineErrorType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain
+{
+    public enum SoundEngineErrorType
+    {
+        FileError = 0,
+        DeviceError = 1,
+    }
+}

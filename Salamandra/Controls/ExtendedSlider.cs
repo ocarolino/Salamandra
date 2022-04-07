1b24362e9c67fa3edbce1c2c68300048dc34e45d
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-07 19:57:08 -0300

    ExtendedSlider c/ CustomTooltipContent

--- Salamandra/Controls/ExtendedSlider.cs
+++ Salamandra/Controls/ExtendedSlider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,7 +13,7 @@ namespace Salamandra.Controls
 {
     public class ExtendedSlider : Slider
     {
-        #region Dependency Properties
+        #region Drag Command Properties
         public ICommand? DragStartedCommand
         {
             get => (ICommand)GetValue(DragStartedCommandProperty);
@@ -33,11 +34,51 @@ namespace Salamandra.Controls
             "DragCompletedCommand", typeof(ICommand), typeof(ExtendedSlider), new PropertyMetadata(null));
         #endregion
 
+        #region Tooltip Properties
+        private ToolTip _autoToolTip;
+        private ToolTip AutoToolTip
+        {
+            get
+            {
+                if (_autoToolTip == null)
+                {
+                    var field = typeof(Slider).GetField(nameof(_autoToolTip), BindingFlags.NonPublic | BindingFlags.Instance);
+                    _autoToolTip = field?.GetValue(this) as ToolTip;
+                }
+
+                return _autoToolTip;
+            }
+        }
+
+        public string CustomToolTipContent
+        {
+            get => (string)GetValue(CustomToolTipContenttProperty);
+            set => SetValue(CustomToolTipContenttProperty, value);
+        }
+
+        public static readonly DependencyProperty CustomToolTipContenttProperty = DependencyProperty.Register(nameof(CustomToolTipContent), typeof(string), typeof(ExtendedSlider), new PropertyMetadata(default(string)));
+        #endregion
+
+        private void FormatAutoToolTipContent()
+        {
+            if (!string.IsNullOrEmpty(CustomToolTipContent))
+                AutoToolTip.Content = CustomToolTipContent;
+        }
+
         protected override void OnThumbDragStarted(DragStartedEventArgs e)
         {
             base.OnThumbDragStarted(e);
 
             this.DragStartedCommand?.Execute(null);
+
+            FormatAutoToolTipContent();
+        }
+
+        protected override void OnThumbDragDelta(DragDeltaEventArgs e)
+        {
+            base.OnThumbDragDelta(e);
+
+            FormatAutoToolTipContent();
         }
 
         protected override void OnThumbDragCompleted(DragCompletedEventArgs e)

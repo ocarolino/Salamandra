d867b143c5906df800cf46d479df094b58eee74d
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-12 19:56:42 -0300

    Classes de domínio para playlist loader

---
+++ Salamandra.Engine/Domain/PlaylistTrackInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain
+{
+    public class PlaylistTrackInfo
+    {
+        public string? Filename { get; set; }
+        public string? FriendlyName { get; set; }
+        public double? Duration { get; set; }
+    }
+}
---
+++ Salamandra.Engine/Exceptions/PlaylistLoaderException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Exceptions
+{
+    public class PlaylistParserException : Exception
+    {
+    }
+}

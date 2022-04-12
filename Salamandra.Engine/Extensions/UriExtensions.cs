f39951f3ba62fc3f23f82f9810360908da7630b0
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-12 20:09:58 -0300

    UriExtensions

---
+++ Salamandra.Engine/Extensions/UriExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Extensions
+{
+    public static class UriExtensions
+    {
+        public static Uri MakeAbsoluteUri(this Uri u, Uri uri)
+        {
+            return new Uri(u, uri);
+        }
+    }
+}

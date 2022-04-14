d9d8eefe997429f9c6b9517a1f57be4273efcf57
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-14 10:24:43 -0300

    FontAwesome e LoadingSpinner

---
+++ Salamandra/UserControls/LoadingSpinner.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Salamandra.UserControls
+{
+    /// <summary>
+    /// Interação lógica para LoadingSpinner.xam
+    /// </summary>
+    public partial class LoadingSpinner : UserControl
+    {
+        public string LoadingText
+        {
+            get { return (string)GetValue(LoadingTextProperty); }
+            set { SetValue(LoadingTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty LoadingTextProperty = DependencyProperty.Register("LoadingText", typeof(string),
+            typeof(LoadingSpinner), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                LoadingTextChanged, null, false, UpdateSourceTrigger.PropertyChanged));
+
+        private static void LoadingTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is LoadingSpinner spinner)
+            {
+                spinner.UpdateText();
+            }
+        }
+
+        public LoadingSpinner()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateText()
+        {
+            TextField.Text = LoadingText;
+        }
+    }
+}

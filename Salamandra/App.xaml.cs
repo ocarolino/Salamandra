c5ca3af3c48891b30921dea2891fa1f26beace74
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-18 09:40:16 -0300

    Método p/ atualizar cultura do App com informações do sistema

--- Salamandra/App.xaml.cs
+++ Salamandra/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace Salamandra
 {
@@ -13,5 +16,25 @@ namespace Salamandra
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SetCultureFromSystem();
+
+            base.OnStartup(e);
+        }
+
+        private static void SetCultureFromSystem()
+        {
+            var vCulture = System.Globalization.CultureInfo.CurrentCulture;
+
+            Thread.CurrentThread.CurrentCulture = vCulture;
+            Thread.CurrentThread.CurrentUICulture = vCulture;
+            CultureInfo.DefaultThreadCurrentCulture = vCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = vCulture;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
     }
 }

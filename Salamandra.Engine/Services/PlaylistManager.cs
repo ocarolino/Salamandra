89e0ac3f2b5ca6888c9ce56f70a5f54c6401d0a1
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-19 12:28:16 -0300

    using namespace das Tracks

--- Salamandra.Engine/Services/PlaylistManager.cs
+++ Salamandra.Engine/Services/PlaylistManager.cs
@@ -9,6 +9,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Salamandra.Engine.Domain.Tracks;
 
 namespace Salamandra.Engine.Services
 {
--- Salamandra/ViewModel/MainViewModel.cs
+++ Salamandra/ViewModel/MainViewModel.cs
@@ -15,6 +15,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
+using Salamandra.Engine.Domain.Tracks;
 
 namespace Salamandra.ViewModel
 {

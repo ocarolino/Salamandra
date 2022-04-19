9c9d8d389aeca3cd2ade3e2c0b3a9d38898ef95c
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-19 12:27:18 -0300

    Reorganização geral das Tracks

---
+++ Salamandra.Engine/Domain/Tracks/MultiFileTrack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain.Tracks
+{
+    public abstract class MultiFileTrack : SingleFileTrack
+    {
+        public List<string> Filenames { get; set; }
+        public int CurrentFileIndex { get; set; }
+
+        public MultiFileTrack()
+        {
+            this.Filenames = new List<string>();
+            this.CurrentFileIndex = -1;
+        }
+
+        public abstract string? GetCurrentFile();
+    }
+}
---
+++ Salamandra.Engine/Domain/Tracks/RotationTrack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain.Tracks
+{
+    public class RotationTrack : MultiFileTrack
+    {
+        public override bool HasTrackFinished => true;
+
+        public override string? GetCurrentFile()
+        {
+            if (this.Filenames.Count == 0)
+                return null;
+
+            if (this.CurrentFileIndex == -1)
+                this.CurrentFileIndex++;
+
+            string file = this.Filenames[this.CurrentFileIndex];
+
+            this.CurrentFileIndex++;
+
+            if (this.CurrentFileIndex >= this.Filenames.Count)
+                this.CurrentFileIndex = 0;
+
+            return file;
+        }
+    }
+}

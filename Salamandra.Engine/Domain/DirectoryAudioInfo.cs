a23cc067f6750c54cffc3f681ae162f76f1aaf82
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-21 18:24:23 -0300

    Implementação de DirectoryAudioInfo

---
+++ Salamandra.Engine/Domain/DirectoryAudioInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain
+{
+    public class DirectoryAudioInfo
+    {
+        public string DirectoryPath { get; set; }
+        public List<string> Files { get; set; }
+        public DateTime LastScanDate { get; set; }
+
+        public DirectoryAudioInfo(string directoryPath)
+        {
+            this.DirectoryPath = directoryPath;
+            this.Files = new List<string>();
+        }
+    }
+}

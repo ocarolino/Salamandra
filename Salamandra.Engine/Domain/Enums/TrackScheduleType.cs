dd135e553e20142a318f78c17b8bb2c7ed277290
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-26 12:18:17 -0300

    Classe de domínio ScheduledEvent

---
+++ Salamandra.Engine/Domain/Enums/TrackScheduleType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain.Enums
+{
+    public enum TrackScheduleType
+    {
+        FileTrack = 0,
+        RandomFileTrack = 1,
+        TimeAnnouncementTrack = 2,
+    }
+}
---
+++ Salamandra.Engine/Domain/Events/ScheduledEvent.cs
@@ -0,0 +1,30 @@
+﻿using Salamandra.Engine.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain.Events
+{
+    public class ScheduledEvent
+    {
+        public int Id { get; set; }
+        public bool Immediate { get; set; }
+        public TimeScheduleType TimeScheduleType { get; set; }
+        public DateTime StartingDateTime { get; set; }
+        public ObservableCollection<int> PlayingHours { get; set; }
+        public DateTime ExpirationDateTime { get; set; }
+        public ObservableCollection<DayOfWeek> DaysOfWeek { get; set; }
+        public TrackScheduleType TrackScheduleType { get; set; }
+        public string Filename { get; set; }
+
+        public ScheduledEvent()
+        {
+            this.PlayingHours = new ObservableCollection<int>();
+            this.DaysOfWeek = new ObservableCollection<DayOfWeek>();
+            this.Filename = String.Empty;
+        }
+    }
+}

6a1f0ea08caeee3e5de950fd64a9674a4a438137
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-26 12:25:53 -0300

    Implementação inicial do EventManager

---
+++ Salamandra.Engine/Services/EventManager.cs
@@ -0,0 +1,19 @@
+﻿using Salamandra.Engine.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Services
+{
+    public class EventManager
+    {
+        public List<ScheduledEvent> Events { get; set; }
+
+        public EventManager()
+        {
+            this.Events = new List<ScheduledEvent>();
+        }
+    }
+}

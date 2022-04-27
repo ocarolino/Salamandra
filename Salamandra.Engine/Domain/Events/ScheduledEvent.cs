30989586e093a71e8f12b50c05a4d237f3b6677c
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-27 10:14:42 -0300

    Propriedades extras no ScheduledEvent

--- Salamandra.Engine/Domain/Events/ScheduledEvent.cs
+++ Salamandra.Engine/Domain/Events/ScheduledEvent.cs
@@ -13,10 +13,13 @@ namespace Salamandra.Engine.Domain.Events
     {
         public int Id { get; set; }
         public bool Immediate { get; set; }
-        public TimeScheduleType TimeScheduleType { get; set; }
+        //public TimeScheduleType TimeScheduleType { get; set; }
         public DateTime StartingDateTime { get; set; }
+        public bool UsePlayingHours { get; set; }
         public ObservableCollection<int> PlayingHours { get; set; }
+        public bool UseExpirationDateTime { get; set; }
         public DateTime ExpirationDateTime { get; set; }
+        public bool UseDaysOfWeek { get; set; }
         public ObservableCollection<DayOfWeek> DaysOfWeek { get; set; }
         public TrackScheduleType TrackScheduleType { get; set; }
         public string Filename { get; set; }

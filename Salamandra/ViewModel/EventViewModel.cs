ff5b8ed46b36cefd657650ca398b0262d6ca751f
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-27 11:59:18 -0300

    Clone p/ edição no EventViewModel

--- Salamandra/ViewModel/EventViewModel.cs
+++ Salamandra/ViewModel/EventViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Salamandra.Engine.Domain.Events;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -10,6 +12,10 @@ namespace Salamandra.ViewModel
 {
     public class EventViewModel : INotifyPropertyChanged
     {
+        private ScheduledEvent? originalScheduledEvent;
+
+        public ScheduledEvent ScheduledEvent { get; set; }
+
         public ObservableCollection<int> Hours { get; set; }
         public ObservableCollection<DayOfWeek> Days { get; set; }
 
@@ -23,6 +29,24 @@ namespace Salamandra.ViewModel
 
             foreach (DayOfWeek item in Enum.GetValues(typeof(DayOfWeek)))
                 this.Days.Add(item);
+
+            this.ScheduledEvent = new ScheduledEvent();
+        }
+
+        public EventViewModel(ScheduledEvent scheduledEvent) : this()
+        {
+            this.originalScheduledEvent = scheduledEvent;
+        }
+
+        public void Loading()
+        {
+            if (this.originalScheduledEvent != null)
+            {
+                var serialized = JsonConvert.SerializeObject(this.originalScheduledEvent);
+                var scheduledEvent = JsonConvert.DeserializeObject<ScheduledEvent>(serialized);
+
+                this.ScheduledEvent = scheduledEvent;
+            }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;

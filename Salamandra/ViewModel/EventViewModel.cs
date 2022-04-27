afb0b0224db8048af215928d7035a3e825df3a37
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-27 10:15:17 -0300

    Implementação inicial da EventWindow e EventViewModel

---
+++ Salamandra/ViewModel/EventViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.ViewModel
+{
+    public class EventViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<int> Hours { get; set; }
+        public ObservableCollection<DayOfWeek> Days { get; set; }
+
+        public EventViewModel()
+        {
+            this.Hours = new ObservableCollection<int>();
+            this.Days = new ObservableCollection<DayOfWeek>();
+
+            for (int i = 0; i < 24; i++)
+                this.Hours.Add(i);
+
+            foreach (DayOfWeek item in Enum.GetValues(typeof(DayOfWeek)))
+                this.Days.Add(item);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
---
+++ Salamandra/Views/EventWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Salamandra.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Salamandra.Views
+{
+    /// <summary>
+    /// Lógica interna para EventWindow.xaml
+    /// </summary>
+    public partial class EventWindow : Window
+    {
+        private EventViewModel eventViewModel;
+
+        public EventWindow(EventViewModel eventViewModel)
+        {
+            InitializeComponent();
+
+            this.eventViewModel = eventViewModel;
+            this.DataContext = this.eventViewModel;
+        }
+    }
+}

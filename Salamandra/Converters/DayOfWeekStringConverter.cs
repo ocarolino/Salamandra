ea64325f558cb534440af672ee723a7439edce3e
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-27 10:14:56 -0300

    DayOfWeekStringConverter

---
+++ Salamandra/Converters/DayOfWeekStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using Salamandra.Engine.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Salamandra.Converters
+{
+    public class DayOfWeekStringConverter : IValueConverter
+    {
+        // ToDo: Lançar um parãmetro pra short
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DayOfWeek dayOfWeek = (DayOfWeek)value;
+            culture = Thread.CurrentThread.CurrentUICulture;
+
+            return culture.DateTimeFormat.GetAbbreviatedDayName(dayOfWeek).FirstCharToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

e04bd842341174ece89232076701dec0845ff970
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-27 21:31:29 -0300

    Create SwitchBindingExtension.cs

---
+++ Salamandra/Converters/SwitchBindingExtension.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Salamandra.Converters
+{
+    // Source: https://stackoverflow.com/questions/841808/wpf-display-a-bool-value-as-yes-no (2022-04-27)
+    public class SwitchBindingExtension : Binding
+    {
+        public SwitchBindingExtension()
+        {
+            Initialize();
+        }
+
+        public SwitchBindingExtension(string path)
+            : base(path)
+        {
+            Initialize();
+        }
+
+        public SwitchBindingExtension(string path, object valueIfTrue, object valueIfFalse)
+            : base(path)
+        {
+            Initialize();
+            this.ValueIfTrue = valueIfTrue;
+            this.ValueIfFalse = valueIfFalse;
+        }
+
+        private void Initialize()
+        {
+            this.ValueIfTrue = Binding.DoNothing;
+            this.ValueIfFalse = Binding.DoNothing;
+            this.Converter = new SwitchConverter(this);
+        }
+
+        [ConstructorArgument("valueIfTrue")]
+        public object ValueIfTrue { get; set; }
+
+        [ConstructorArgument("valueIfFalse")]
+        public object ValueIfFalse { get; set; }
+
+        private class SwitchConverter : IValueConverter
+        {
+            public SwitchConverter(SwitchBindingExtension switchExtension)
+            {
+                _switch = switchExtension;
+            }
+
+            private SwitchBindingExtension _switch;
+
+            #region IValueConverter Members
+
+            public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                try
+                {
+                    bool b = System.Convert.ToBoolean(value);
+                    return b ? _switch.ValueIfTrue : _switch.ValueIfFalse;
+                }
+                catch
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                return Binding.DoNothing;
+            }
+
+            #endregion
+        }
+    }
+}

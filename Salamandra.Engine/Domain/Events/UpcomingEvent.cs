907949a469327256f8c95bbbf5531e6bd7e8d09b
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-04-29 12:31:47 -0300

    Implementação do UpcomingEvent

---
+++ Salamandra.Engine/Domain/Events/UpcomingEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain.Events
+{
+    public class UpcomingEvent
+    {
+        public int EventId { get; set; }
+        public DateTime StartTime { get; set; }
+        public bool Imediate { get; set; }
+        public BaseTrack? Track { get; set; }
+    }
+}

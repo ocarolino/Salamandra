6295560082e924e763ee413bf22c34429a939654
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-05-12 09:47:44 -0300

    Novas propriedades adicionadas na classe PlayerSettings

--- Salamandra.Engine/Domain/Settings/PlayerSettings.cs
+++ Salamandra.Engine/Domain/Settings/PlayerSettings.cs
@@ -13,12 +13,21 @@ namespace Salamandra.Engine.Domain.Settings
         public float Volume { get; set; }
         public PlaylistMode PlaylistMode { get; set; }
         public bool AskToCloseWhenPlaying { get; set; }
+        public bool EnableEvents { get; set; }
+        public bool OpenLastPlaylistOnStartup { get; set; }
+        public bool LastPlaylist { get; set; }
+        public bool ShufflePlaylistOnStartup { get; set; }
+        public bool PlayOnStartup { get; set; }
 
         public PlayerSettings()
         {
             this.Volume = 1;
             this.PlaylistMode = PlaylistMode.Default;
             this.AskToCloseWhenPlaying = true;
+            this.EnableEvents = true;
+            this.LastPlaylist = string.Empty;
+            this.ShufflePlaylistOnStartup = false;
+            this.PlayOnStartup = false;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;

684b35676cd79b7756b3b4ea7854107f7cf2f27d
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-05-20 09:35:38 -0300

    RandomFileTrack herdando de SingleFileTrack

--- Salamandra.Engine/Domain/Tracks/RandomFileTrack.cs
+++ Salamandra.Engine/Domain/Tracks/RandomFileTrack.cs
@@ -6,24 +6,12 @@ using System.Threading.Tasks;
 
 namespace Salamandra.Engine.Domain.Tracks
 {
-    public class RandomFileTrack : MultiFileTrack
+    public class RandomFileTrack : SingleFileTrack
     {
         public override bool HasTrackFinished => true;
 
         public RandomFileTrack() : base()
         {
-        }
-
-        public override string? GetFile()
-        {
-            // ToDo: Evitar com que arquivos se repitam.
-
-            if (this.Filenames.Count == 0)
-                return null;
-
-            Random random = new Random();
-
-            return this.Filenames[random.Next(this.Filenames.Count)];
         }
     }
 }

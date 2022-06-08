4ae241e7e40d81452c720d478f9ea8b0dc9e8fb4
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-06-08 12:59:54 -0300

    Implementação básica do LogManager

---
+++ Salamandra.Engine/Services/LogManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Salamandra.Engine.Extensions;
+using Serilog;
+using Serilog.Core;
+
+namespace Salamandra.Engine.Services
+{
+    // Based on: https://github.com/thecodrr/BreadPlayer/blob/add082d5d3ed3da472791f9a5040920a6084a937/BreadPlayer.Common/BLogger.cs (2022-06-08)
+    public class LogManager : IDisposable
+    {
+        public ILogger? Logger { get; set; }
+        public string OutputFolder { get; set; }
+
+        public LogManager(string outputFolder)
+        {
+            this.OutputFolder = outputFolder;
+        }
+
+        public void InitializeLog()
+        {
+            var outputTemplate = "[{Timestamp:HH:mm:ss.fff} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+            var path = Path.Combine(this.OutputFolder, "PlayerSalamandra-.txt");
+
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.File(path,
+                    outputTemplate: outputTemplate,
+                    rollingInterval: RollingInterval.Day,
+                    shared: true)
+                .CreateLogger();
+
+            this.Logger = logger;
+        }
+
+        public void Information(string message, params object[] propertyValues)
+        {
+            this.Logger?.Information(message, propertyValues);
+        }
+
+        public void Exception(string message, Exception? exception = null, params object[] propertyValues)
+        {
+            this.Logger?.Error(exception, message, propertyValues);
+        }
+
+        public void Dispose()
+        {
+            if (this.Logger != null)
+                (this.Logger as IDisposable)!.Dispose();
+        }
+    }
+}

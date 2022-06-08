f9242f30ac72392b3149d6a956320e1b0a7f80e7
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-06-07 21:01:57 -0300

    InverseBooleanConverter

---
+++ Salamandra/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Salamandra.Converters
+{
+    // Source: https://stackoverflow.com/questions/1039636/how-to-bind-inverse-boolean-properties-in-wpf - 2021-09-01
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}

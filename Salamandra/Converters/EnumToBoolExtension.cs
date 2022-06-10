54c8a178ecd5c8ac3aa60276c4594269022cd9a0
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-06-10 10:09:37 -0300

    EnumToBoolExtension

---
+++ Salamandra/Converters/EnumToBoolExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Salamandra.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Enum))]
+    public class EnumToBoolExtension : MarkupExtension, IValueConverter
+    {
+        #region IValueConverter
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return parameter.Equals(value);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value) == true ? parameter : DependencyProperty.UnsetValue;
+        }
+        #endregion
+
+        #region MarkupExtension
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+        #endregion
+    }
+}

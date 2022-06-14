ea7248e0acff7a70b86fd09e79b57859f1a88534
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-06-14 19:57:46 -0300

    Uso do EnumDescriptionTypeConverter em TimeScheduleType e TrackScheduleType

--- Salamandra.Engine/Domain/Enums/PlaylistMode.cs
+++ Salamandra.Engine/Domain/Enums/PlaylistMode.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Salamandra.Engine.Converter;
+using System.ComponentModel;
 
 namespace Salamandra.Engine.Domain.Enums
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum PlaylistMode
     {
+        [Description("Padrão")]
         Default = 0,
+        [Description("Repetir")]
         Repeat = 1,
+        [Description("Aleatório")]
         Random = 2,
+        [Description("Manual")]
         Manual = 3,
+        [Description("Embaralhar")]
         Shuffle = 4,
     }
 }
--- Salamandra.Engine/Domain/Enums/TrackScheduleType.cs
+++ Salamandra.Engine/Domain/Enums/TrackScheduleType.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Salamandra.Engine.Converter;
+using System.ComponentModel;
 
 namespace Salamandra.Engine.Domain.Enums
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum TrackScheduleType
     {
+        [Description("Faixa de Áudio")]
         AudioFileTrack = 0,
+        [Description("Arquivo Aleatório de Pasta")]
         RandomFileTrack = 1,
+        [Description("Locução de Hora")]
         TimeAnnouncementTrack = 2,
+        [Description("Iniciar Playlist")]
         StartPlaylistTrack = 3,
+        [Description("Parar Playlist")]
         StopPlaylistTrack = 4,
+        [Description("Arquivo de Playlist")]
         OpenPlaylistTrack = 5,
     }
 }

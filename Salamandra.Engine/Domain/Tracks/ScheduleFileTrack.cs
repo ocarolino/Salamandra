6f8a5efb476354c0fd63a174c03da065af64ea85
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-06-20 12:45:07 -0300

    Implementação do ScheduleFileTrack

---
+++ Salamandra.Engine/Domain/Tracks/ScheduleFileTrack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain.Tracks
+{
+    public class ScheduleFileTrack : SingleFileTrack
+    {
+        public override bool HasTrackFinished => true;
+    }
+}

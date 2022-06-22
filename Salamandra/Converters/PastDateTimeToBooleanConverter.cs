5f6ad15b88dbd1851c00325d5fd28d789587767a
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-06-22 10:02:50 -0300

    Estilo para eventos expirados na EventListWindow

---
+++ Salamandra/Converters/PastDateTimeToBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Salamandra.Converters
+{
+    public class PastDateTimeToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is DateTime dateTime && dateTime < DateTime.Now ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

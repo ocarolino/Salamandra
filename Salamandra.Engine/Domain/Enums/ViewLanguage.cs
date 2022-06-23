afa405bb8bf21dde3839b90e0562d7a982bcb6ce
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-06-23 09:42:20 -0300

    Implementada configuração para idioma

---
+++ Salamandra.Engine/Domain/Enums/ViewLanguage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Domain.Enums
+{
+    public enum ViewLanguage
+    {
+        Portuguese = 0,
+        English = 1,
+    }
+}

075d005db50c604a80de94aadcfd2dd5cdc0ae9f
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-06-27 21:40:21 -0300

    Implementação de GetDialogFilterFromArray para gerar um filtro a partir de uma array de extensões

---
+++ Salamandra.Engine/Extensions/ArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salamandra.Engine.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static string GetDialogFilterFromArray(this string[] input, string description)
+        {
+            string[] filter = new string[input.Length];
+
+            for (int i = 0; i < filter.Length; i++)
+                filter[i] = "*" + input[i];
+
+            return String.Format("{0} ({1}) | {2}", description, String.Join(", ", filter), String.Join(";", filter));
+        }
+    }
+}

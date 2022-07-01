51869d72c740bc894192bdda35c0d2bcc51cfc52
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-07-01 19:47:42 -0300

    DayOfWeekStringConverter c/ parâmetro para longo ou curto

--- Salamandra/Converters/DayOfWeekStringConverter.cs
+++ Salamandra/Converters/DayOfWeekStringConverter.cs
@@ -12,13 +12,17 @@ namespace Salamandra.Converters
 {
     public class DayOfWeekStringConverter : IValueConverter
     {
-        // ToDo: Lançar um parãmetro pra short
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             DayOfWeek dayOfWeek = (DayOfWeek)value;
             culture = Thread.CurrentThread.CurrentUICulture;
 
-            return culture.DateTimeFormat.GetAbbreviatedDayName(dayOfWeek).FirstCharToUpper();
+            bool longName = parameter is bool ? (bool)parameter : false;
+
+            if (longName)
+                return culture.DateTimeFormat.GetDayName(dayOfWeek).FirstCharToUpper();
+            else
+                return culture.DateTimeFormat.GetAbbreviatedDayName(dayOfWeek).FirstCharToUpper();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)

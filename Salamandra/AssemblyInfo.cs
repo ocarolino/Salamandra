f8888cfc0bc51caf1d70c7a1992ce94aa59a6d65
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-07-11 08:48:17 -0300

    Informações de Assembly

--- Salamandra/AssemblyInfo.cs
+++ Salamandra/AssemblyInfo.cs
@@ -1,4 +1,7 @@
+using System.Reflection;
 using System.Windows;
+
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("windows7.0")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -8,3 +11,14 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: AssemblyTitle("Carolino.Salamandra")]
+[assembly: AssemblyDescription("Radio automation software")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Matheus Carolino")]
+[assembly: AssemblyProduct("Salamandra")]
+[assembly: AssemblyCopyright("Copyright © Matheus Carolino 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")] 

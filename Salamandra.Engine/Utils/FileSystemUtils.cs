a24dd3a02a06e19ba2c5c5df229b34e83ba740c3
ocarolino <74987364+ocarolino@users.noreply.github.com>
2022-07-23 09:20:06 -0300

    Implementação do DetectFileEncoding no FileSystemUtils

--- Salamandra.Engine/Utils/FileSystemUtils.cs
+++ Salamandra.Engine/Utils/FileSystemUtils.cs
@@ -12,5 +12,38 @@ namespace Salamandra.Engine.Utils
         {
             return AppContext.BaseDirectory;
         }
+
+        // Source: https://stackoverflow.com/questions/1025332/determine-a-strings-encoding-in-c-sharp (2022-07-23 - Berthier Lemieux)
+        public static Encoding DetectFileEncoding(Stream fileStream)
+        {
+            var Utf8EncodingVerifier = Encoding.GetEncoding("utf-8", new EncoderExceptionFallback(), new DecoderExceptionFallback());
+
+            using (var reader = new StreamReader(fileStream, Utf8EncodingVerifier,
+                   detectEncodingFromByteOrderMarks: true, leaveOpen: true, bufferSize: 1024))
+            {
+                string detectedEncoding;
+
+                try
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                    }
+
+                    detectedEncoding = reader.CurrentEncoding.BodyName;
+                }
+                catch (Exception e)
+                {
+                    // Failed to decode the file using the BOM/UT8. 
+                    // Assume it's local ANSI
+                    detectedEncoding = "ISO-8859-1";
+                }
+
+                // Rewind the stream
+                fileStream.Seek(0, SeekOrigin.Begin);
+
+                return Encoding.GetEncoding(detectedEncoding);
+            }
+        }
     }
 }
